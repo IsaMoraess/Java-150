33c336056d6dc0f0f0e153a0a479f4c6c06b8335
IsaMoraess <135661414+IsaMoraess@users.noreply.github.com>
2024-12-14 09:52:35 -0300

    Add files via upload

---
+++ Controlador.java
@@ -0,0 +1,12 @@
+public interface Controlador{
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abirmenu();
+    public abstract void fecharmenu();
+    public abstract void maisvolume();
+    public abstract void menosvolume();
+    public abstract void ligarMudo();
+    public abstract void desligarMudo();
+    public abstract void play();
+    public abstract void pause();
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //Construtor
+        ControleRemoto c = new ControleRemoto();
+        //METODOS DISPONIVEIS NA INTERFACE
+        c.ligar();
+        c.maisvolume();
+        c.ligarMudo();
+        c.abirmenu();
+
+    }
+}
